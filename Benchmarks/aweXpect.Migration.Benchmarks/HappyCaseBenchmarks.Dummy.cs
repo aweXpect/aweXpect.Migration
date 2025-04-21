c9380a1fb3fd6d8bbca753212880f5aa82f6b654
Valentin Breuß <vbreuss@gmail.com>
2025-04-21 06:30:01 +0200

    Initial commit

---
+++ Benchmarks/aweXpect.Migration.Benchmarks/HappyCaseBenchmarks.Dummy.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace aweXpect.Migration.Benchmarks;
+
+/// <summary>
+///     This is a dummy benchmark in the Migration template.
+/// </summary>
+public partial class HappyCaseBenchmarks
+{
+	[Benchmark]
+	public TimeSpan Dummy_aweXpect()
+		=> TimeSpan.FromSeconds(10);
+}
