d9ba94af3a89a6a80f69b39dbed9596ae6fcea55
Valentin Breuß <vbreuss@gmail.com>
2025-04-21 06:50:15 +0200

    refactor: remove the benchmark project

--- Pipeline/Build.cs
+++ Pipeline/Build.cs
@@ -27,5 +27,5 @@ partial class Build : NukeBuild
 	AbsolutePath TestResultsDirectory => RootDirectory / "TestResults";
 	GitHubActions GitHubActions => GitHubActions.Instance;
 
-	public static int Main() => Execute<Build>(x => x.ApiChecks, x => x.Benchmarks, x => x.CodeAnalysis);
+	public static int Main() => Execute<Build>(x => x.ApiChecks, x => x.CodeAnalysis);
 }
