887170d8965847a3766d47bb284f1e035ef6c2eb
Valentin Breuß <vbreuss@gmail.com>
2025-04-27 06:21:12 +0200

    feat: improve xunit `Assert.Equal` with tolerance migration
    Support the cases when the third parameter is a number or a `TimeSpan` tolerance

---
+++ Tests/aweXpect.Migration.Tests/Xunit/TestCases.cs
@@ -0,0 +1,185 @@
+﻿namespace aweXpect.Migration.Tests.Xunit;
+
+public static class TestCases
+{
+	/// <summary>
+	///     <see href="https://github.com/xunit/assert.xunit/tree/main" />
+	/// </summary>
+	public static TheoryData<string, string, string, bool> Basic()
+	{
+		TheoryData<string, string, string, bool> theoryData = new();
+		theoryData.AddTestCase("",
+			"Assert.Fail(\"foo\")",
+			"Fail.Test(\"foo\")");
+		theoryData.AddTestCase("",
+			"Assert.Skip(\"foo\")",
+			"Skip.Test(\"foo\")");
+		theoryData.AddTestCase("",
+			"Assert.Null(new object())",
+			"Expect.That(new object()).IsNull()");
+		theoryData.AddTestCase("",
+			"Assert.NotNull(new object())",
+			"Expect.That(new object()).IsNotNull()");
+		theoryData.AddTestCase("",
+			"Assert.Same(new ArgumentException(), new NullReferenceException())",
+			"Expect.That(new NullReferenceException()).IsSameAs(new ArgumentException())");
+		theoryData.AddTestCase("",
+			"Assert.NotSame(new ArgumentException(), new NullReferenceException())",
+			"Expect.That(new NullReferenceException()).IsNotSameAs(new ArgumentException())");
+		return theoryData;
+	}
+
+	/// <summary>
+	///     <see href="https://github.com/xunit/assert.xunit/blob/main/BooleanAsserts.cs" />
+	/// </summary>
+	public static TheoryData<string, string, string, bool> Boolean()
+	{
+		TheoryData<string, string, string, bool> theoryData = new();
+		theoryData.AddTestCase("bool subject = false;",
+			"Assert.True(subject)",
+			"Expect.That(subject).IsTrue()");
+		theoryData.AddTestCase("bool subject = false;",
+			"Assert.False(subject)",
+			"Expect.That(subject).IsFalse()");
+		theoryData.AddTestCase("bool subject = false;",
+			"Assert.True(subject, \"foo\")",
+			"Expect.That(subject).IsTrue().Because(\"foo\")");
+		theoryData.AddTestCase("bool subject = false;",
+			"Assert.False(subject, \"foo\")",
+			"Expect.That(subject).IsFalse().Because(\"foo\")");
+		return theoryData;
+	}
+
+	/// <summary>
+	///     <see href="https://github.com/xunit/assert.xunit/blob/main/CollectionAsserts.cs" />
+	/// </summary>
+	public static TheoryData<string, string, string, bool> Collections()
+	{
+		TheoryData<string, string, string, bool> theoryData = new();
+		theoryData.AddTestCase("",
+			"Assert.Distinct([1, 2,])",
+			"Expect.That([1, 2,]).AreAllUnique()");
+		return theoryData;
+	}
+
+	/// <summary>
+	///     <see href="https://github.com/xunit/assert.xunit/blob/main/EqualityAsserts.cs" />
+	/// </summary>
+	public static TheoryData<string, string, string, bool> Equality()
+	{
+		TheoryData<string, string, string, bool> theoryData = new();
+		theoryData.AddTestCase("",
+			"Assert.Equal(1, 2)",
+			"Expect.That(2).IsEqualTo(1)");
+		theoryData.AddTestCase("",
+			"Assert.Equal(1.0, 1.1, 0.1)",
+			"Expect.That(1.1).IsEqualTo(1.0).Within(0.1)");
+		theoryData.AddTestCase("",
+			"Assert.Equal(DateTime.Now, DateTime.Today, TimeSpan.FromSeconds(1))",
+			"Expect.That(DateTime.Today).IsEqualTo(DateTime.Now).Within(TimeSpan.FromSeconds(1))");
+		theoryData.AddTestCase("",
+			"Assert.NotEqual(1, 2)",
+			"Expect.That(2).IsNotEqualTo(1)");
+		theoryData.AddTestCase("",
+			"Assert.NotEqual(1.0, 1.1, 0.1)",
+			"Expect.That(1.1).IsNotEqualTo(1.0).Within(0.1)");
+		return theoryData;
+	}
+
+	/// <summary>
+	///     <see href="https://github.com/xunit/assert.xunit/blob/main/ExceptionAsserts.cs" />
+	/// </summary>
+	public static TheoryData<string, string, string, bool> Exceptions()
+	{
+		TheoryData<string, string, string, bool> theoryData = new();
+		theoryData.AddTestCase("Action callback = () => {};",
+			"Assert.ThrowsAny<ArgumentException>(callback)",
+			"Expect.That(callback).Throws<ArgumentException>()");
+		theoryData.AddTestCase("Func<Task> callback = () => Task.CompletedTask;",
+			"Assert.ThrowsAnyAsync<ArgumentException>(callback)",
+			"Expect.That(callback).Throws<ArgumentException>()");
+		theoryData.AddTestCase("Action callback = () => {};",
+			"Assert.Throws<ArgumentException>(callback)",
+			"Expect.That(callback).ThrowsExactly<ArgumentException>()");
+		theoryData.AddTestCase("Func<Task> callback = () => Task.CompletedTask;",
+			"Assert.ThrowsAsync<ArgumentException>(callback)",
+			"Expect.That(callback).ThrowsExactly<ArgumentException>()");
+		theoryData.AddTestCase("Action callback = () => {};",
+			"Assert.Throws(typeof(ArgumentException), callback)",
+			"Expect.That(callback).ThrowsExactly(typeof(ArgumentException))");
+		theoryData.AddTestCase("Func<Task> callback = () => Task.CompletedTask;",
+			"Assert.ThrowsAsync(typeof(ArgumentException), callback)",
+			"Expect.That(callback).ThrowsExactly(typeof(ArgumentException))");
+		return theoryData;
+	}
+
+	/// <summary>
+	///     <see href="https://github.com/xunit/assert.xunit/blob/main/StringAsserts.cs" />
+	/// </summary>
+	public static TheoryData<string, string, string, bool> Strings()
+	{
+		TheoryData<string, string, string, bool> theoryData = new();
+		theoryData.AddTestCase("",
+			"Assert.Contains(\"oo\", \"foo\")",
+			"Expect.That(\"foo\").Contains(\"oo\")");
+		theoryData.AddTestCase("",
+			"Assert.DoesNotContain(\"oo\", \"foo\")",
+			"Expect.That(\"foo\").DoesNotContain(\"oo\")");
+		theoryData.AddTestCase("",
+			"Assert.StartsWith(\"fo\", \"foo\")",
+			"Expect.That(\"foo\").StartsWith(\"fo\")");
+		theoryData.AddTestCase("",
+			"Assert.EndsWith(\"oo\", \"foo\")",
+			"Expect.That(\"foo\").EndsWith(\"oo\")");
+		theoryData.AddTestCase("",
+			"Assert.Empty(\"foo\")",
+			"Expect.That(\"foo\").IsEmpty()");
+		theoryData.AddTestCase("",
+			"Assert.NotEmpty(\"foo\")",
+			"Expect.That(\"foo\").IsNotEmpty()");
+		return theoryData;
+	}
+
+	/// <summary>
+	///     <see href="https://github.com/xunit/assert.xunit/blob/main/TypeAsserts.cs" />
+	/// </summary>
+	public static TheoryData<string, string, string, bool> Types()
+	{
+		TheoryData<string, string, string, bool> theoryData = new();
+		theoryData.AddTestCase("",
+			"Assert.IsAssignableFrom<ArgumentException>(new Exception())",
+			"Expect.That(new Exception()).Is<ArgumentException>()");
+		theoryData.AddTestCase("",
+			"Assert.IsAssignableFrom(typeof(ArgumentException), new Exception())",
+			"Expect.That(new Exception()).Is(typeof(ArgumentException))");
+		theoryData.AddTestCase("",
+			"Assert.IsNotAssignableFrom<ArgumentException>(new Exception())",
+			"Expect.That(new Exception()).IsNot<ArgumentException>()");
+		theoryData.AddTestCase("",
+			"Assert.IsNotAssignableFrom(typeof(ArgumentException), new Exception())",
+			"Expect.That(new Exception()).IsNot(typeof(ArgumentException))");
+		theoryData.AddTestCase("",
+			"Assert.IsType<ArgumentException>(new Exception())",
+			"Expect.That(new Exception()).IsExactly<ArgumentException>()");
+		theoryData.AddTestCase("",
+			"Assert.IsType(typeof(ArgumentException), new Exception())",
+			"Expect.That(new Exception()).IsExactly(typeof(ArgumentException))");
+		theoryData.AddTestCase("",
+			"Assert.IsNotType<ArgumentException>(new Exception())",
+			"Expect.That(new Exception()).IsNotExactly<ArgumentException>()");
+		theoryData.AddTestCase("",
+			"Assert.IsNotType(typeof(ArgumentException), new Exception())",
+			"Expect.That(new Exception()).IsNotExactly(typeof(ArgumentException))");
+		return theoryData;
+	}
+
+	private static void AddTestCase(this TheoryData<string, string, string, bool> theoryData,
+		string arrange,
+		string actual,
+		string expected,
+		bool isAsync = false)
+		=> theoryData.Add(
+			actual,
+			expected,
+			arrange, isAsync);
+}
--- Tests/aweXpect.Migration.Tests/Xunit/XunitAssertionCodeFixProviderTests.cs
+++ Tests/aweXpect.Migration.Tests/Xunit/XunitAssertionCodeFixProviderTests.cs
@@ -7,105 +7,60 @@ namespace aweXpect.Migration.Tests.Xunit;
 public class XunitAssertionCodeFixProviderTests
 {
 	[Theory]
-	[InlineData("Assert.True(false)",
-		"Expect.That(false).IsTrue()")]
-	[InlineData("Assert.False(true)",
-		"Expect.That(true).IsFalse()")]
-	[InlineData("Assert.True(false, \"foo\")",
-		"Expect.That(false).IsTrue().Because(\"foo\")")]
-	[InlineData("Assert.False(true, \"foo\")",
-		"Expect.That(true).IsFalse().Because(\"foo\")")]
-	[InlineData("Assert.Null(new object())",
-		"Expect.That(new object()).IsNull()")]
-	[InlineData("Assert.NotNull(new object())",
-		"Expect.That(new object()).IsNotNull()")]
-	[InlineData("Assert.Equal(1, 2)",
-		"Expect.That(2).IsEqualTo(1)")]
-	[InlineData("Assert.NotEqual(1, 2)",
-		"Expect.That(2).IsNotEqualTo(1)")]
-	[InlineData("Assert.Contains(\"oo\", \"foo\")",
-		"Expect.That(\"foo\").Contains(\"oo\")")]
-	[InlineData("Assert.DoesNotContain(\"oo\", \"foo\")",
-		"Expect.That(\"foo\").DoesNotContain(\"oo\")")]
-	[InlineData("Assert.StartsWith(\"fo\", \"foo\")",
-		"Expect.That(\"foo\").StartsWith(\"fo\")")]
-	[InlineData("Assert.EndsWith(\"oo\", \"foo\")",
-		"Expect.That(\"foo\").EndsWith(\"oo\")")]
-	[InlineData("Assert.Empty(\"foo\")",
-		"Expect.That(\"foo\").IsEmpty()")]
-	[InlineData("Assert.NotEmpty(\"foo\")",
-		"Expect.That(\"foo\").IsNotEmpty()")]
-	[InlineData("Assert.Distinct([1, 2,])",
-		"Expect.That([1, 2,]).AreAllUnique()")]
-	[InlineData("Assert.Same(new ArgumentException(), new NullReferenceException())",
-		"Expect.That(new NullReferenceException()).IsSameAs(new ArgumentException())")]
-	[InlineData("Assert.NotSame(new ArgumentException(), new NullReferenceException())",
-		"Expect.That(new NullReferenceException()).IsNotSameAs(new ArgumentException())")]
-	[InlineData("Assert.IsAssignableFrom<ArgumentException>(new Exception())",
-		"Expect.That(new Exception()).Is<ArgumentException>()")]
-	[InlineData("Assert.IsAssignableFrom(typeof(ArgumentException), new Exception())",
-		"Expect.That(new Exception()).Is(typeof(ArgumentException))")]
-	[InlineData("Assert.IsNotAssignableFrom<ArgumentException>(new Exception())",
-		"Expect.That(new Exception()).IsNot<ArgumentException>()")]
-	[InlineData("Assert.IsNotAssignableFrom(typeof(ArgumentException), new Exception())",
-		"Expect.That(new Exception()).IsNot(typeof(ArgumentException))")]
-	[InlineData("Assert.IsType<ArgumentException>(new Exception())",
-		"Expect.That(new Exception()).IsExactly<ArgumentException>()")]
-	[InlineData("Assert.IsType(typeof(ArgumentException), new Exception())",
-		"Expect.That(new Exception()).IsExactly(typeof(ArgumentException))")]
-	[InlineData("Assert.IsNotType<ArgumentException>(new Exception())",
-		"Expect.That(new Exception()).IsNotExactly<ArgumentException>()")]
-	[InlineData("Assert.IsNotType(typeof(ArgumentException), new Exception())",
-		"Expect.That(new Exception()).IsNotExactly(typeof(ArgumentException))")]
-	[InlineData("Assert.Fail(\"foo\")",
-		"Fail.Test(\"foo\")")]
-	[InlineData("Assert.Skip(\"foo\")",
-		"Skip.Test(\"foo\")")]
-	[InlineData("Assert.ThrowsAny<ArgumentException>((Action)(() => {}))",
-		"Expect.That((Action)(() => {})).Throws<ArgumentException>()")]
-	[InlineData("Assert.ThrowsAnyAsync<ArgumentException>((Func<Task>)(() => Task.CompletedTask))",
-		"Expect.That((Func<Task>)(() => Task.CompletedTask)).Throws<ArgumentException>()")]
-	[InlineData("Assert.Throws<ArgumentException>((Action)(() => {}))",
-		"Expect.That((Action)(() => {})).ThrowsExactly<ArgumentException>()")]
-	[InlineData("Assert.ThrowsAsync<ArgumentException>((Func<Task>)(() => Task.CompletedTask))",
-		"Expect.That((Func<Task>)(() => Task.CompletedTask)).ThrowsExactly<ArgumentException>()")]
-	[InlineData("Assert.Throws(typeof(ArgumentException), (Action)(() => {}))",
-		"Expect.That((Action)(() => {})).ThrowsExactly(typeof(ArgumentException))")]
-	[InlineData("Assert.ThrowsAsync(typeof(ArgumentException), (Func<Task>)(() => Task.CompletedTask))",
-		"Expect.That((Func<Task>)(() => Task.CompletedTask)).ThrowsExactly(typeof(ArgumentException))")]
-	public async Task ShouldApplyCodeFix(string xunitAssertion, string aweXpectExpectation) => await Verifier
-		.VerifyCodeFixAsync(
-			$$"""
-			  using System;
-			  using System.Threading.Tasks;
-			  using aweXpect;
-			  using Xunit;
+	[MemberData(nameof(TestCases.Basic), MemberType = typeof(TestCases))]
+	public async Task ShouldApplyCodeFixForBasicTestCases(
+		string xunitAssertion,
+		string aweXpect,
+		string arrange,
+		bool isAsync) => await VerifyTestCase(xunitAssertion, aweXpect, arrange, isAsync);
 
-			  public class MyClass
-			  {
-			      [Fact]
-			      public void MyTest()
-			      {
-			          [|{{xunitAssertion}}|];
-			      }
-			  }
-			  """,
-			$$"""
-			  using System;
-			  using System.Threading.Tasks;
-			  using aweXpect;
-			  using Xunit;
+	[Theory]
+	[MemberData(nameof(TestCases.Boolean), MemberType = typeof(TestCases))]
+	public async Task ShouldApplyCodeFixForBooleanTestCases(
+		string xunitAssertion,
+		string aweXpect,
+		string arrange,
+		bool isAsync) => await VerifyTestCase(xunitAssertion, aweXpect, arrange, isAsync);
 
-			  public class MyClass
-			  {
-			      [Fact]
-			      public void MyTest()
-			      {
-			          {{aweXpectExpectation}};
-			      }
-			  }
-			  """
-		);
+	[Theory]
+	[MemberData(nameof(TestCases.Collections), MemberType = typeof(TestCases))]
+	public async Task ShouldApplyCodeFixForCollectionTestCases(
+		string xunitAssertion,
+		string aweXpect,
+		string arrange,
+		bool isAsync) => await VerifyTestCase(xunitAssertion, aweXpect, arrange, isAsync);
+
+	[Theory]
+	[MemberData(nameof(TestCases.Equality), MemberType = typeof(TestCases))]
+	public async Task ShouldApplyCodeFixForEqualityTestCases(
+		string xunitAssertion,
+		string aweXpect,
+		string arrange,
+		bool isAsync) => await VerifyTestCase(xunitAssertion, aweXpect, arrange, isAsync);
+
+	[Theory]
+	[MemberData(nameof(TestCases.Exceptions), MemberType = typeof(TestCases))]
+	public async Task ShouldApplyCodeFixForExceptionTestCases(
+		string xunitAssertion,
+		string aweXpect,
+		string arrange,
+		bool isAsync) => await VerifyTestCase(xunitAssertion, aweXpect, arrange, isAsync);
+
+	[Theory]
+	[MemberData(nameof(TestCases.Strings), MemberType = typeof(TestCases))]
+	public async Task ShouldApplyCodeFixForStringTestCases(
+		string xunitAssertion,
+		string aweXpect,
+		string arrange,
+		bool isAsync) => await VerifyTestCase(xunitAssertion, aweXpect, arrange, isAsync);
+
+	[Theory]
+	[MemberData(nameof(TestCases.Types), MemberType = typeof(TestCases))]
+	public async Task ShouldApplyCodeFixForTypeTestCases(
+		string xunitAssertion,
+		string aweXpect,
+		string arrange,
+		bool isAsync) => await VerifyTestCase(xunitAssertion, aweXpect, arrange, isAsync);
 
 	[Fact]
 	public async Task ShouldApplyCodeFixInTheory() => await Verifier
@@ -142,5 +97,50 @@ public class XunitAssertionCodeFixProviderTests
 			    }
 			}
 			"""
+		);
+
+
+	private static async Task VerifyTestCase(
+		string xunitAssertion,
+		string aweXpect,
+		string arrange,
+		bool isAsync) => await Verifier
+		.VerifyCodeFixAsync(
+			$$"""
+			  using System;
+			  using System.Collections.Generic;
+			  using System.Threading.Tasks;
+			  using aweXpect;
+			  using Xunit;
+
+			  public class MyClass
+			  {
+			      [Fact]
+			      public {{(isAsync ? "async Task" : "void")}} MyTest()
+			      {
+			          {{arrange}}
+			          
+			          {{(isAsync ? "await " : "")}}[|{{xunitAssertion}}|];
+			      }
+			  }
+			  """,
+			$$"""
+			  using System;
+			  using System.Collections.Generic;
+			  using System.Threading.Tasks;
+			  using aweXpect;
+			  using Xunit;
+
+			  public class MyClass
+			  {
+			      [Fact]
+			      public {{(isAsync ? "async Task" : "void")}} MyTest()
+			      {
+			          {{arrange}}
+			          
+			          {{(isAsync ? "await " : "")}}{{aweXpect}};
+			      }
+			  }
+			  """
 		);
 }
